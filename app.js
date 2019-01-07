bc26defc3452be783ae96701b43ab790794a2280
geo2004 <46329778+geo2004@users.noreply.github.com>
2019-01-07 10:00:57 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+new Vue({
+  el: '#app',
+  data: { /* Data properties will go here */ },
+  mounted() { /* Code to run when app is mounted */ },
+  methods: { /* Any app-specific functions go here */ },
+});
