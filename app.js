6e315664f8d0c6c6555929cef23ab8f01f7963e8
geo2004 <46329778+geo2004@users.noreply.github.com>
2019-01-07 14:54:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -217,42 +217,49 @@ new Vue({
 ],
   },],
 },
- mounted() {
-  this.initMap();
-  this.initLayers();
-},
-  methods: {
-  initMap() {this.map = L.map('map').setView([38.63, -90.23], 12);
-this.tileLayer = L.tileLayer(
-  'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
-  {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
-  }
-);
-this.tileLayer.addTo(this.map);
-            },
-  initLayers() {this.layers.forEach((layer) => {
-   const markerFeatures = layer.features.filter(feature => feature.type === 'marker');
-const polygonFeatures = layer.features.filter(feature => feature.type === 'polygon');
-    markerFeatures.forEach((feature) => {
-  feature.leafletObject = L.marker(feature.coords)
-    .bindPopup(feature.name);
-});
-    polygonFeatures.forEach((feature) => {
-  feature.leafletObject = L.polygon(feature.coords)
-    .bindPopup(feature.name);
-});
-  
+mounted() {
+    this.initMap();
+    this.initLayers();
   },
-    
-    layerChanged(layerId, active) {const layer = this.layers.find(layer => layer.id === layerId);
-         layer.features.forEach((feature) => {
- if (active) {
-  feature.leafletObject.addTo(this.map);
-} else {
-  feature.leafletObject.removeFrom(this.map);
-}
-});      }
-},
+  methods: {
+    layerChanged(layerId, active) {
+      const layer = this.layers.find(layer => layer.id === layerId);
+      
+      layer.features.forEach((feature) => {
+        if (active) {
+          feature.leafletObject.addTo(this.map);
+        } else {
+          feature.leafletObject.removeFrom(this.map);
+        }
+      });
+    },
+    initLayers() {
+      this.layers.forEach((layer) => {
+        const markerFeatures = layer.features.filter(feature => feature.type === 'marker');
+        const polygonFeatures = layer.features.filter(feature => feature.type === 'polygon');
+        
+        markerFeatures.forEach((feature) => {
+          feature.leafletObject = L.marker(feature.coords)
+            .bindPopup(feature.name);
+        });
+        
+        polygonFeatures.forEach((feature) => {
+          feature.leafletObject = L.polygon(feature.coords)
+            .bindPopup(feature.name);
+        });
+      });
+    },
+    initMap() {
+      this.map = L.map('map').setView([38.63, -90.23], 12);
+      this.tileLayer = L.tileLayer(
+        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+        {
+          maxZoom: 18,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+        }
+      );
+        
+      this.tileLayer.addTo(this.map);
+    },
+  },
 });
