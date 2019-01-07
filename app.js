60cc3867c4a5d0e8a83df6013e94b8db11636823
geo2004 <46329778+geo2004@users.noreply.github.com>
2019-01-07 10:22:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 new Vue({
   el: '#app',
-  data: { /* Data properties will go here */ },
-  mounted() { /* Code to run when app is mounted */ },
-  methods: { /* Any app-specific functions go here */ },
+  data: {
+  map: null,
+  tileLayer: null,
+  layers: [],
+},
+ mounted() {
+  this.initMap();
+  this.initLayers();
+},
+  methods: {
+  initMap() {this.map = L.map('map').setView([38.63, -90.23], 12);
+this.tileLayer = L.tileLayer(
+  'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+  {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+  }
+);
+this.tileLayer.addTo(this.map);
+            },
+  initLayers() {},
+},
 });
